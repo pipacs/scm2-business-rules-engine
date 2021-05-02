15bda42226c4857214c842633bc02702d735b468
Akos Polster <akos@pipacs.com>
2021-05-02 21:48:54 +0200

    Add more Product types.

--- products.py
+++ products.py
@@ -9,3 +9,30 @@ class PhysicalProduct(Product):
         """A physical product"""
         pass
 
+
+class Book(PhysicalProduct):
+        """A book"""
+        pass 
+
+
+class Membership(Product):
+        """A membeship with an owner"""
+
+        def __init__(self, owner):
+                self.owner = owner
+
+
+class Upgrade(Product):
+        """An uprgade to a membership"""
+
+        def __init__(self, owner):
+                self.owner = owner
+
+
+class Video(PhysicalProduct):
+        """A (physical) video"""
+
+        def __init__(self, title):
+                self.title = title 
+
+
